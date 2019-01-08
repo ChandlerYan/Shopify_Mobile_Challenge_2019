bc074d85f677c71dbc92d7687f163124f71aa042
Yucheng Yan <yanyucheng007@hotmail.com>
2019-01-08 16:44:00 -0500

    Initial Commit

---
+++ Shopify Mobile Challenge Winter 2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopify Mobile Challenge Winter 2019
+//
+//  Created by Yucheng Yan on 2019-01-08.
+//  Copyright © 2019 Yucheng Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
